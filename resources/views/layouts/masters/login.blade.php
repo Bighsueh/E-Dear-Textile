dcb30727bc73bc7dc9c7a4e247c58763c8836dfb
Hsueh <40841121@gm.nfu.edu.tw>
2021-09-02 22:48:24 +0800

    add login page

---
+++ resources/views/layouts/masters/login.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="zh-tw">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" >
+    <title>123</title>
+</head>
+<body class="bg-light">
+    @yield('content')
+</body>
+<!-- Bootstrap core JavaScript -->
+<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+</html>
