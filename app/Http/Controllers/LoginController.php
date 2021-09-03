2011f0539fcdd916b98b5ac52271676c6733644d
Hsueh <40841121@gm.nfu.edu.tw>
2021-09-03 16:50:59 +0800

    add LoginController post_login level判斷

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
 use mysql_xdevapi\Exception;
 
 class LoginController extends Controller
@@ -20,14 +21,29 @@ class LoginController extends Controller
 
         $users_table = DB::table('users')->get();
         try {
-            $check_account = $users_table
-                            ->where('account', $employee_id)
-                            ->where('password',$employee_password)
-                            ->toArray();
-            if (count($check_account) == 0 ){
+            $account_info = $users_table
+                ->where('account', $employee_id)
+                ->where('password', $employee_password)
+                ->first();
+
+//            帳戶不存在
+            if ($account_info == null) {
                 return redirect()->route('get_login');
             };
-            dd($check_account);
+
+//            使用者權限判斷
+            $level = $account_info->level;
+            switch ($level) {
+                case 'admin':
+                    Session::put('level', 'admin');
+                case 'manager':
+                    Session::put('level', 'manager');
+                case 'employee':
+                    Session::put('level', 'employee');
+            }
+
+            dd($level);
+//            return page right here
         } catch (Exception $exception) {
             return redirect()->route('get_login');
         }
