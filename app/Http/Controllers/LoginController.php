ac72b769d3d738b4f8a6e936bfd2795175edd68b
Hsueh <40841121@gm.nfu.edu.tw>
2021-09-03 10:57:03 +0800

    add login method

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use mysql_xdevapi\Exception;
 
 class LoginController extends Controller
 {
@@ -13,6 +15,21 @@ class LoginController extends Controller
 
     public function post_login(Request $request)
     {
-        dd($request->input('employee_id'));
+        $employee_id = $request->input('employee_id');
+        $employee_password = $request->input('employee_password');
+
+        $users_table = DB::table('users')->get();
+        try {
+            $check_account = $users_table
+                            ->where('account', $employee_id)
+                            ->where('password',$employee_password)
+                            ->toArray();
+            if (count($check_account) == 0 ){
+                return redirect()->route('get_login');
+            };
+            dd($check_account);
+        } catch (Exception $exception) {
+            return redirect()->route('get_login');
+        }
     }
 }
