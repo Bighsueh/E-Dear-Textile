30a1bd8a14d71cad626ffb376264ae5730e8795f
Bighsueh <40841121@gm.nfu.edu.tw>
2021-09-13 20:09:37 +0800

    set login.blade.php title to "益得紡織登入介面"

--- resources/views/layouts/masters/login.blade.php
+++ resources/views/layouts/masters/login.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" >
-    <title>123</title>
+    <title>益得紡織登入介面</title>
 </head>
 <body class="bg-light">
     @yield('content')
