1ce94841e37a63667bfd8de8285f90851616d3ee
Bighsueh <40841121@gm.nfu.edu.tw>
2021-10-03 03:23:23 +0800

    add navbar of manager.blade.php

--- resources/views/layouts/masters/manager.blade.php
+++ resources/views/layouts/masters/manager.blade.php
@@ -9,6 +9,31 @@
     <title>益得紡織幹部介面</title>
 </head>
 <body class="bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand text-white" href="#">益得紡織-幹部管理介面</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="text-light nav-link active" aria-current="page" href="{{Route('get_menu')}}">派遣單列表</a>
+                </li>
+                <li class="nav-item">
+                    <a class="text-light nav-link" href="{{Route('get_addSheet')}}">新增派遣單</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="text-secondary nav-link" href="#">登出</a>
+                </li>
+{{--                <li class="nav-item">--}}
+{{--                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>--}}
+{{--                </li>--}}
+            </ul>
+        </div>
+    </div>
+</nav>
     @yield('content')
 </body>
 <!-- Bootstrap core JavaScript -->
