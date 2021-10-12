b9980cd1909b4390fce1d02beb88b43c6fb725cd
yoyo <ya90312@gmail.com>
2021-10-12 21:16:35 +0800

    變更表格

--- resources/views/pages/employee/employeeMenu.blade.php
+++ resources/views/pages/employee/employeeMenu.blade.php
@@ -1,5 +1,5 @@
-@extends('layouts.masters.manager')
-{{--改--}}
+@extends('layouts.masters.employee')
+
 @section('content')
     <div style="display: flex" class="container">
         <table class="table">
@@ -10,10 +10,29 @@
                 <th>貨號</th>
                 <th>訂單數量</th>
                 <th>回報</th>
-                <th>狀態</th>
             </tr>
             </thead>
             <tbody>
+            @foreach($job_tickets as $job_ticket)
+                <tr>
+                    <td>
+                        {{$job_ticket->employeeName}}
+                    </td>
+                    <td>
+                        {{$job_ticket->id}}
+                    </td>
+                    <td>
+                        {{$job_ticket->item}}
+                    </td>
+                    <td>
+                        {{$job_ticket->order.'打'}}
+                    </td>
+                    <td>
+                        {{--選填還沒帶值--}}
+                        <button type="button" class="btn btn-secondary">選填</button>
+                    </td>
+                </tr>
+            @endforeach
             </tbody>
 
         </table>
