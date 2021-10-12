7d4ce3aa80e2321eb1ce3280e5d8843826022999
yoyo <ya90312@gmail.com>
2021-10-12 20:02:15 +0800

    add employeeMenu.blade.php

---
+++ resources/views/pages/employee/employeeMenu.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.masters.manager')
+{{--改--}}
+@section('content')
+    <div style="display: flex" class="container">
+        <table class="table">
+            <thead class="thead-dark">
+            <tr>
+                <th>客戶名稱</th>
+                <th>派遣單編號</th>
+                <th>貨號</th>
+                <th>訂單數量</th>
+                <th>回報</th>
+                <th>狀態</th>
+            </tr>
+            </thead>
+            <tbody>
+            </tbody>
+
+        </table>
+
+    </div>
+
+
+@endsection
