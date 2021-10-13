115f284dc58058e66ee3ac12acca7e9befa1d77c
“{gayZ3A20}” <{gay195587@gmail.com}>
2021-10-13 23:52:23 +0800

    <script>$('#qrcode').qrcode("");</script> -> Uncaught ReferenceError: $ is not defined

--- resources/views/pages/manager/menu.blade.php
+++ resources/views/pages/manager/menu.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.masters.manager')
 @section('content')
+
     <div style="display: flex" class="container">
         <table class="table">
             <thead class="thead-dark">
@@ -34,6 +35,40 @@
                     <td>
                         {{$job_ticket->status}}
                     </td>
+                    <td>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                            QR Code
+                        </button>
+                        <script>
+
+
+                            $('#qrcode').qrcode("http://140.130.36.85");
+
+
+
+                        </script>
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">{{$job_ticket->employeeName}}</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+
+                                        <div id="qrcode"></div>
+
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                   </td>
                 </tr>
             @endforeach
             </tbody>
@@ -41,6 +76,7 @@
         </table>
 
     </div>
+
 
 
 @endsection
