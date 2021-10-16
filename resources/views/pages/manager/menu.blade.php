7d16bdd45444564efaf804eee06700d8ff6cf44b
“{gayZ3A20}” <{gay195587@gmail.com}>
2021-10-16 18:33:44 +0800

    "http://140.130.36.85/"+ {{$job_ticket->id}}

--- resources/views/pages/manager/menu.blade.php
+++ resources/views/pages/manager/menu.blade.php
@@ -44,18 +44,19 @@
                         <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                             <div class="modal-dialog" role="document">
                                 <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLabel">{{$job_ticket->employeeName}}</h5>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                    <div class="modal-body">
-
-                                        <div id="qrcode">
-
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">{{$job_ticket->employeeName}}</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
                                         </div>
-
+                                    <div class="modal-body">
+                                        <div class="d-flex justify-content-center">
+                                            <div id="qrcode"></div>
+                                            <script>
+                                                $('#qrcode').qrcode("http://140.130.36.85/"+ {{$job_ticket->id}});
+                                            </script>
+                                        </div>
                                     </div>
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -71,9 +72,7 @@
         </table>
 
     </div>
-    <script>
-        $('#qrcode').qrcode("http://140.130.36.85");
-    </script>
+
 
 
 @endsection
