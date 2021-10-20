ba7ada425720a6a08af5c913d5a99a751cc2f9af
Hsueh <40841121@gm.nfu.edu.tw>
2021-10-20 16:22:12 +0800

    add 員工QRcode

--- resources/views/layouts/masters/employee.blade.php
+++ resources/views/layouts/masters/employee.blade.php
@@ -7,6 +7,20 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
     <title>益得紡織員工介面</title>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <script type="text/javascript" src="{{ asset('assets/js/jquery-3.6.0.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/jquery.qrcode.min.js') }}"></script>
+    <script src="{{ asset('assets/js/qrcode.js') }}"></script>
+
 </head>
 <body class="bg-light h-100">
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -23,12 +37,25 @@
                        href="{{Route('get_employee_menu')}}">派遣單列表</a>
                 </li>
                 <li class="nav-item">
+                    <a class="text-light nav-link active" aria-current="page"
+                    onclick="employeeQrcodeButtonClick()">員工QR code</a>
+                </li>
+                <li class="nav-item">
                     <a class="text-secondary nav-link" href="{{Route('get_login')}}">登出</a>
                 </li>
                 {{--                <li class="nav-item">--}}
                 {{--                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>--}}
                 {{--                </li>--}}
             </ul>
+            <button type="button" id="employeeQrcodeButton" data-toggle="modal" data-target="#EmployeeQrcode"
+                    style="display: none;visibility: hidden"></button>
+            <script>
+                // employeeQrcodeButton click
+                function employeeQrcodeButtonClick(){
+                    document.getElementById('employeeQrcodeButton').click();
+                }
+
+            </script>
         </div>
     </div>
 </nav>
@@ -45,12 +72,31 @@
 
     </div>
 </div>
-
+<!-- Modal -->
+<div class="modal fade" id="EmployeeQrcode" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header ">
+                <h5 class="modal-title" id="exampleModalLabel" style="">員工QR code</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="d-flex justify-content-center">
+                    <div id="employeeQrcodeImg"></div>
+                    <script>
+                        $("#employeeQrcodeImg").qrcode("http://140.130.36.85");
+                    </script>
+                </div>
+            </div>
+            <div class="modal-footer">
+                {{--                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>--}}
+            </div>
+        </div>
+    </div>
+</div>
 
 </body>
 
-<!-- Bootstrap core JavaScript -->
-<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </html>
