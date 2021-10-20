1983ad91de7c743eb60813b212847e0ef07336dd
Hsueh <40841121@gm.nfu.edu.tw>
2021-10-20 15:47:32 +0800

    remark modal footer close button

--- resources/views/pages/manager/menu.blade.php
+++ resources/views/pages/manager/menu.blade.php
@@ -47,9 +47,9 @@
                                 <div class="modal-content">
                                         <div class="modal-header ">
                                             <h5 class="modal-title" id="exampleModalLabel" style="">{{$job_ticket->employeeName}}</h5>
-{{--                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">--}}
-{{--                                                <span aria-hidden="true">&times;</span>--}}
-{{--                                            </button>--}}
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
                                         </div>
                                     <div class="modal-body">
                                         <div class="d-flex justify-content-center">
@@ -60,7 +60,7 @@
                                         </div>
                                     </div>
                                     <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+{{--                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>--}}
                                     </div>
                                 </div>
                             </div>
