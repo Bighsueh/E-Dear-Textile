4d023a3319482ad2cba5a40d242f284dcd814fa5
Hsueh <40841121@gm.nfu.edu.tw>
2021-10-20 16:29:23 +0800

    move 派遣單QRcode from manager->employee

--- resources/views/pages/employee/employeeMenu.blade.php
+++ resources/views/pages/employee/employeeMenu.blade.php
@@ -30,14 +30,41 @@
                     <td>
                         {{--選填還沒帶值--}}
                         <button type="button" class="btn btn-secondary">選填</button>
+                        <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#QRcodeModal-{{$job_ticket->id}}">
+                            QR Code
+                        </button>
                     </td>
                 </tr>
+                <!-- Modal -->
+                <div class="modal fade" id="QRcodeModal-{{$job_ticket->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header ">
+                                <h5 class="modal-title" id="exampleModalLabel" style="">{{$job_ticket->employeeName}}</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="d-flex justify-content-center">
+                                    <div id="qrcode-{{$job_ticket->id}}"></div>
+                                    <script>
+                                        $('#qrcode-{{$job_ticket->id}}').qrcode("http://140.130.36.85/"+ {{$job_ticket->id}});
+                                    </script>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                {{--                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>--}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             @endforeach
             </tbody>
 
         </table>
 
     </div>
-
 
 @endsection
--- resources/views/pages/manager/menu.blade.php
+++ resources/views/pages/manager/menu.blade.php
@@ -36,36 +36,7 @@
                     <td>
                         {{$job_ticket->status}}
                     </td>
-                    <td>
-                        <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#QRcodeModal-{{$job_ticket->id}}">
-                            QR Code
-                        </button>
 
-                        <!-- Modal -->
-                        <div class="modal fade" id="QRcodeModal-{{$job_ticket->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                        <div class="modal-header ">
-                                            <h5 class="modal-title" id="exampleModalLabel" style="">{{$job_ticket->employeeName}}</h5>
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </div>
-                                    <div class="modal-body">
-                                        <div class="d-flex justify-content-center">
-                                            <div id="qrcode-{{$job_ticket->id}}"></div>
-                                            <script>
-                                                $('#qrcode-{{$job_ticket->id}}').qrcode("http://140.130.36.85/"+ {{$job_ticket->id}});
-                                            </script>
-                                        </div>
-                                    </div>
-                                    <div class="modal-footer">
-{{--                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>--}}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                   </td>
                 </tr>
             @endforeach
             </tbody>
