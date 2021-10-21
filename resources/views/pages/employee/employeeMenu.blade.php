13012c4453702e1083e32f2c65c0a0df9e0bc461
yoyo <ya90312@gmail.com>
2021-10-21 16:59:13 +0800

    讓選填按鈕可以使用

--- resources/views/pages/employee/employeeMenu.blade.php
+++ resources/views/pages/employee/employeeMenu.blade.php
@@ -28,8 +28,47 @@
                         {{$job_ticket->order.'打'}}
                     </td>
                     <td>
+                        <form class="form" method="POST" action="{{route('post_employee_report')}}">
+                            @csrf
+                            <!-- 回報的Modal -->
+                            <div class="modal fade" id="report{{$job_ticket->id}}" tabindex="-1" role="dialog" aria-labelledby="reportModal" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <!-- head -->
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="resultModal">選擇回報方式</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <!-- body -->
+                                        <input type="hidden" value="{{$job_ticket->id}}" name="report_ticket_id">
+                                        <div class="modal-body">
+                                            {{$job_ticket->id}}
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="radio" name="report_rd" id="report_rd1" value="1" >
+                                                <label class="form-check-label" for="report_rd1">
+                                                    剪巾回報
+                                                </label>
+                                            </div>
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="radio" name="report_rd" id="report_rd2"  value="2" checked>
+                                                <label class="form-check-label" for="report_rd2">
+                                                    折頭回報
+                                                </label>
+                                            </div>
+                                        </div>
+                                        <!-- footer -->
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">關閉</button>
+                                            <button type="submit" class="btn btn-primary">進入回報</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
                         {{--選填還沒帶值--}}
-                        <button type="button" class="btn btn-secondary">選填</button>
+                        <button type="button" data-toggle="modal" data-target="#report{{$job_ticket->id}}" class="btn btn-secondary">選填</button>
                         <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#QRcodeModal-{{$job_ticket->id}}">
                             QR Code
                         </button>
