cefd69e54866bd1e96224c11ec0890df5d13d1f2
Bighsueh <40841121@gm.nfu.edu.tw>
2021-10-25 22:53:38 +0800

    add OpenScanner function

--- app/Http/Controllers/ScannerController.php
+++ app/Http/Controllers/ScannerController.php
@@ -2,21 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class ScannerController extends Controller
 {
-    public function CutToPiping(Request $request)
+    public function OpenScanner(Request $request,$qr_code_status)
     {
+        $user_id = $request->session()->get('user_id');
+        $level = $request->session()->get('level');
 
+        Session::put('qr_code_status',$qr_code_status);
+
+        return redirect('app://open');
     }
 
-    public function ManagerToPiping(Request $request)
-    {
 
-    }
 
     public function ManagerToFoldHead(Request $request)
+    {
+
+    }
+
+    public function CutToPiping(Request $request)
     {
 
     }
