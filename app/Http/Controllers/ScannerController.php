d3a0dc3eb4caf62ade8939c12650e843483c231f
Bighsueh <40841121@gm.nfu.edu.tw>
2021-10-25 10:20:56 +0800

    add ScannerController

---
+++ app/Http/Controllers/ScannerController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ScannerController extends Controller
+{
+    public function CutToPiping(Request $request)
+    {
+
+    }
+
+    public function ManagerToPiping(Request $request)
+    {
+
+    }
+
+    public function ManagerToFoldHead(Request $request)
+    {
+
+    }
+}
