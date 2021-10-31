115c129d0f12bea0088f570e14918570b649e760
yoyo <ya90312@gmail.com>
2021-11-01 00:15:28 +0800

    增加測資

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+//      幹部
         DB::table('users')->insert([
             'user_id' =>'40841145',
             'name' =>'yoyo',
@@ -18,12 +19,36 @@ class UsersTableSeeder extends Seeder
             'password' =>'456',
             'level' =>'manager',
         ]);
-
+//      滾邊
         DB::table('users')->insert([
             'user_id' =>'408411455',
             'name' =>'yoyooo',
             'account' =>'456',
             'password' =>'789',
+            'level' =>'employee',
+        ]);
+//      滾邊2
+        DB::table('users')->insert([
+            'user_id' =>'4084114552',
+            'name' =>'yoyooo2',
+            'account' =>'4566',
+            'password' =>'7899',
+            'level' =>'employee',
+        ]);
+//      剪巾
+        DB::table('users')->insert([
+            'user_id' =>'408411455cut',
+            'name' =>'yoyocut',
+            'account' =>'456cut',
+            'password' =>'789cut',
+            'level' =>'employee',
+        ]);
+//      剪巾2
+        DB::table('users')->insert([
+            'user_id' =>'408411455cut2',
+            'name' =>'yoyocut2',
+            'account' =>'4566cut',
+            'password' =>'7899cut',
             'level' =>'employee',
         ]);
     }
