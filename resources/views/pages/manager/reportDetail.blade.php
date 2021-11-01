15fc34373970e4a980297b16303ff805d718f414
yoyo <ya90312@gmail.com>
2021-11-02 00:04:06 +0800

    新增回報細項介面及route

---
+++ resources/views/pages/manager/reportDetail.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.masters.manager')
+@section('content')
+    <div class="container">
+        <table class="table">
+            <thead class="thead-dark">
+            <th scope="col">項目</th>
+            <th scope="col">總數量</th>
+            <th scope="col">作業員</th>
+            <th scope="col">數量</th>
+            </thead>
+        </table>
+    </div>
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::group(['middleware' =>['login']],function(){
     Route::post('/menu/addSheet/create', 'MenuController@post_create_addSheet')->name('post_create_addSheet');
     Route::patch('/menu/patchSheet', 'MenuController@patch_patchSheet')->name('patch_patchSheet');
     Route::post('/menu/result','MenuController@get_result')->name('get_result');
+    Route::get('/menu/result/{report}/{id}','MenuController@get_resultDetail')->name('get_resultDetail');
     Route::get('/menu/employeeList','MenuController@get_employeeList')->name('get_employeeList');
     // 員工
     Route::get('/employee/menu', 'EmployeeMenuController@get_employee_menu')->name('get_employee_menu');
