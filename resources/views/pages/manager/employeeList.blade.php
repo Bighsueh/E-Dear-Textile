41955d0e07c67d2cce0858c063c3622662f06839
Bighsueh <40841121@gm.nfu.edu.tw>
2021-11-21 22:45:59 +0800

    add 新增使用者modal及ajax

--- resources/views/pages/manager/employeeList.blade.php
+++ resources/views/pages/manager/employeeList.blade.php
@@ -37,6 +37,53 @@
         </table>
 
     </div>
+    <!-- Modal -->
+    <div class="modal fade" id="CreateUserModal" tabindex="-1" role="dialog" aria-labelledby="CreateUserModalTitle"
+         aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="CreateUserModalLongTitle">新增使用者</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="container">
+                        <div class="row form-group">
+                            <label for="create_level" class="col-sm-4 text-right">選擇職位</label>
+                            <select class="form-control col-sm-6" aria-label="Default select example"
+                                    name="create_level" id="create_level">
+                                <option value="employee">員工</option>
+                                <option value="manager">幹部</option>
+                            </select>
+                        </div>
+                        <div class="row form-group">
+                            <label for="create_name" class="col-sm-4 text-right">請輸入名稱</label>
+                            <input type="text" name="create_name" id="create_name" class="form-control col-sm-6"
+                                   placeholder="員工名稱"/>
+                        </div>
+                        <div class="row form-group">
+                            <label for="create_account" class="col-sm-4 text-right">請輸入帳號</label>
+                            <input type="text" name="create_account" id="create_account" class="form-control col-sm-6"
+                                   placeholder="帳號"/>
+                        </div>
+                        <div class="row form-group">
+                            <label for="create_password" class="col-sm-4 text-right">請輸入密碼</label>
+                            <input type="text" name="create_password" id="create_password" class="form-control col-sm-6"
+                                   placeholder="密碼"/>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-success" id="btn_create_user">新增使用者</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <script>
         let setting_mode = "list";
 
@@ -82,14 +129,11 @@
                             "<tr><td colspan='4' class='text-center'>查無資料</td></tr>"
                         );
                     }
-
-
                 },
                 error: function (err) {
                     console.log(err);
                 }
-            })
-
+            });
         }
 
         $("#btn_setting").click(function () {
@@ -108,6 +152,34 @@
             update_data(search_parameter);
         })
 
+        $("#btn_create_user").click(function () {
+            $.ajax({
+                url: '{{route('create_user_data')}}',
+                method: 'post',
+                data: {
+                    _token: "{{ csrf_token() }}",
+                    create_level: $("#create_level").val(),
+                    create_name: $("#create_name").val(),
+                    create_account: $("#create_account").val(),
+                    create_password: $("#create_password").val(),
+                },
+                success: function (res) {
+                    if (res === "success") {
+                        window.alert("新增成功");
+                    }
+                    $("#create_level").val('employee');
+                    $("#create_name").val('');
+                    $("#create_account").val('');
+                    $("#create_password").val('');
+                    update_data();
+                },
+                error: function (err) {
+                    window.alert('新增失敗：\n' + err['responseJSON']["message"]);
+                    console.log(err['responseJSON']["message"]);
+
+                }
+            })
+        })
 
     </script>
 
