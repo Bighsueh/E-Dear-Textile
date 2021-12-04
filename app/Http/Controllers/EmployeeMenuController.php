ae67f8bf04d9daea44277e7d40c199570b3f43eb
Bighsueh <40841121@gm.nfu.edu.tw>
2021-12-04 21:51:16 +0800

    為滾邊,剪巾回報設定 get_report_data()

--- app/Http/Controllers/EmployeeMenuController.php
+++ app/Http/Controllers/EmployeeMenuController.php
@@ -13,8 +13,8 @@ class EmployeeMenuController extends Controller
         // 派遣單查詢,join job_titles 檢查權限 增加一行where指令(不確定是否需要)
         $job_tickets = DB::table('job_tickets')
             ->join('job_titles', 'job_tickets.id', "=", 'job_titles.ticket_id')
-            ->where('job_titles.authorized_person',session()->get('user_id'))
-            ->where('job_tickets.status','=','排程中')
+            ->where('job_titles.authorized_person', session()->get('user_id'))
+            ->where('job_tickets.status', '=', '排程中')
             ->get();
 //        dd($job_tickets);
 
@@ -22,13 +22,13 @@ class EmployeeMenuController extends Controller
         Session::forget('qr_code_status');
         Session::forget('ticket_info');
 
-        return view('pages.employee.employeeMenu',compact('job_tickets',$job_tickets));
+        return view('pages.employee.employeeMenu', compact('job_tickets', $job_tickets));
     }
 
     public function get_employee_list($id)
     {
-        $job_tickets = DB::table('job_tickets')->where('id',$id)->first();
-        return view('pages.employee.employeeList',compact('job_tickets',$job_tickets));
+        $job_tickets = DB::table('job_tickets')->where('id', $id)->first();
+        return view('pages.employee.employeeList', compact('job_tickets', $job_tickets));
     }
 
     public function post_employee_report(Request $request)
@@ -37,133 +37,138 @@ class EmployeeMenuController extends Controller
 
 //        dd($user_id);
 //        dd($request);
-        $job_title = DB::table('job_titles')->select('title')->where('authorized_person', $user_id)->Where('ticket_id',$request->ticket_id)->orderBy('id','desc')->first();
+        $job_title = DB::table('job_titles')->select('title')->where('authorized_person', $user_id)->Where('ticket_id', $request->ticket_id)->orderBy('id', 'desc')->first();
 //        dd($job_title);
 
         $ticket_id = $request->except('_token');
-        $job_ticket = DB::table('job_tickets')->where('id',$ticket_id)->first();
-        $reports = DB::table('job_reports')->where('ticket_id',$ticket_id)->get();
-        $foldHeadReports = DB::table('job_foldhead_reports')->where('ticket_id',$ticket_id)->get();
+        $job_ticket = DB::table('job_tickets')->where('id', $ticket_id)->first();
+        $reports = DB::table('job_reports')->where('ticket_id', $ticket_id)->get();
+        $foldHeadReports = DB::table('job_foldhead_reports')->where('ticket_id', $ticket_id)->get();
 
 //        計算目前回報總數量
-        if($reports->count())
-        {
+        if ($reports->count()) {
             $sumReports = DB::table('job_reports')->where('ticket_id', $ticket_id)->sum('cut_order');
             $sumPipReports = DB::table('job_reports')->where('ticket_id', $ticket_id)->sum('Piping_order');
 
         }
-        if($foldHeadReports->count())
-        {
+        if ($foldHeadReports->count()) {
             $sumFoldHeadReports = DB::table('job_foldhead_reports')->where('ticket_id', $ticket_id)->sum('foldHead_order');
             $sumPickReports = DB::table('job_foldhead_reports')->where('ticket_id', $ticket_id)->sum('pickTower_order');
 //            dd($sumPickReports);
         }
 
-        if($job_title->title == "剪巾"  ) {
+        if ($job_title->title == "剪巾") {
             $job_tickets = DB::table('job_tickets')->where('id', $request->ticket_id)->get();
             $Pipings = DB::table('job_titles')->where('title', "滾邊")->Where('ticket_id', $request->ticket_id)->Where('authorized_person', $request->authorizer)->get();
 
 //            dd($Pipings);
-            return view('pages.employee.employeeReport',compact('job_tickets','job_title','Pipings','sumReports','sumPipReports','reports'));
-        }
-        elseif($job_title->title == "折頭"){
+            return view('pages.employee.employeeReport', compact('job_tickets', 'job_title', 'Pipings', 'sumReports', 'sumPipReports', 'reports'));
+        } elseif ($job_title->title == "折頭") {
             $job_tickets = DB::table('job_tickets')->where('id', $request->ticket_id)->get();
 //            dd($job_tickets);
             $foldHeads = DB::table('job_titles')->where('title', "折頭")->Where('ticket_id', $request->ticket_id)->get();
 //            dd($foldHead);
-            return view('pages.employee.employeeReport',compact('job_tickets','job_title','foldHeads','sumFoldHeadReports','sumPickReports','foldHeadReports'));
-        }
-        else{
+            return view('pages.employee.employeeReport', compact('job_tickets', 'job_title', 'foldHeads', 'sumFoldHeadReports', 'sumPickReports', 'foldHeadReports'));
+        } else {
             return redirect()->route('get_employee_menu');
         }
     }
 
     public function get_report_data(Request $request)
     {
-        try{
-
+        try {
+            $result = [];
             $user_id = Session::get('user_id');
             //自己回報的
-            $ticket_report = DB::table('job_reports')
-                ->where('ticket_id',$request->ticket_id)
-                ->where('operator',$user_id)
+            $ticket_reports = DB::table('job_reports')
+                ->where('ticket_id', $request->ticket_id)
+                ->where('operator', $user_id)
+                ->join('job_tickets', 'job_reports.ticket_id', '=', 'job_tickets.id')
+                ->join('users', 'users.id', '=', 'job_reports.operator')
+                ->select(
+                    'job_reports.operator', 'job_reports.quantity', 'job_reports.unit','job_reports.ticket_id','job_reports.updated_at',
+                    'job_tickets.employeeName', 'job_tickets.color', 'job_tickets.wash', 'job_tickets.colorId', 'job_tickets.colorId2',
+                    'users.name'
+                )
                 ->get();
 
-            if($request->action == '剪巾'){
+            if ($request->action == '剪巾') {
                 //以回報滾邊
-                $piping_report = DB::table('job_reports')
-                ->where('ticket_id',1)
-                ->where('submit_by',$user_id)
-                    ->where('action','滾邊')
+                $piping_reports = DB::table('job_reports')
+                    ->where('ticket_id', $request->ticket_id)
+                    ->where('submit_by', $user_id)
+                    ->where('action', '滾邊')
+                    ->select('job_reports.operator','job_reports.quantity','job_reports.unit')
                     ->get();
                 //授權給自己的滾邊員
-                $pipings = DB::table('job_titles')
-                    ->where('ticket_id',$request->ticket_id)
-                    ->where('authorized_person',$user_id)
+                $piping_members = DB::table('job_titles')
+                    ->where('ticket_id', $request->ticket_id)
+                    ->where('authorized_person', $user_id)
+                    ->join('users','job_titles.authorizer','=','users.id')
+                    ->select('users.name','users.id')
                     ->get();
 
-
-            }
-            elseif($request->action == '折頭'){
+                $result = [
+                    "ticket_reports" => $ticket_reports,
+                    "piping_reports" => $piping_reports,
+                    "piping_members" => $piping_members
+                ];
+            } elseif ($request->action == '折頭') {
                 //以回報撿巾
                 $pick_report = DB::table('job_reports')
-                    ->where('ticket_id',1)
-                    ->where('submit_by',$user_id)
-                    ->where('action','撿巾')
+                    ->where('ticket_id', 1)
+                    ->where('submit_by', $user_id)
+                    ->where('action', '撿巾')
                     ->get();
                 //自己授權的撿巾員
                 $picks = DB::table('job_titles')
-                    ->where('ticket_id',$request->ticket_id)
-                    ->where('authorizer',$user_id)
+                    ->where('ticket_id', $request->ticket_id)
+                    ->where('authorizer', $user_id)
                     ->get();
             }
 
-
-
-
-
-
-
-        }catch(Exception $exception){
+            return $result;
+        } catch (Exception $exception) {
             return $exception;
         }
     }
-    public function store_report_data(Request $request){
-        try{
+
+    public function store_report_data(Request $request)
+    {
+        try {
 //            $ticket_id = $request->;
 
 
-            return ;
-        }catch (Exception $exception){
-            return$exception;
+            return;
+        } catch (Exception $exception) {
+            return $exception;
         }
     }
+
     public function post_create_employee_report(Request $request)
     {
 //        dd($request);
         $query = $request->except('_token');
 //        dd($query['pick_complete_bar_orders']+$query['pick_complete_dozen_orders']*12);
-        if($query['title'] == '剪巾')
-        {
+        if ($query['title'] == '剪巾') {
             DB::table('job_reports')->insert([
-                'Piping'=>$query['Piping'],
-                'piping_order'=>$query['cut_complete_bar_orders']+$query['cut_complete_dozen_orders']*12,
-                'cut_order' => $query['cut_complete_bar_orders']+$query['cut_complete_dozen_orders']*12,
-                'user_id'=>$request->session()->get('user_id'),
+                'Piping' => $query['Piping'],
+                'piping_order' => $query['cut_complete_bar_orders'] + $query['cut_complete_dozen_orders'] * 12,
+                'cut_order' => $query['cut_complete_bar_orders'] + $query['cut_complete_dozen_orders'] * 12,
+                'user_id' => $request->session()->get('user_id'),
                 'ticket_id' => $query['ticket_id'],
-                'created_at'=>$query['date'],
-                'updated_at'=>$query['date'],
+                'created_at' => $query['date'],
+                'updated_at' => $query['date'],
             ]);
-        }
-        elseif ($query['title'] == '折頭'){
+        } elseif ($query['title'] == '折頭') {
             DB::table('job_foldhead_reports')->insert([
-                'pickTower'=>$query['pick_cloth_emp'],
-                'foldHead_order'=>$query['complete_bar_orders']+$query['complete_dozen_orders']*12,
-                'pickTower_order' => $query['pick_complete_bar_orders']+$query['pick_complete_dozen_orders']*12,
-                'user_id'=>$request->session()->get('user_id'),
+                'pickTower' => $query['pick_cloth_emp'],
+                'foldHead_order' => $query['complete_bar_orders'] + $query['complete_dozen_orders'] * 12,
+                'pickTower_order' => $query['pick_complete_bar_orders'] + $query['pick_complete_dozen_orders'] * 12,
+                'user_id' => $request->session()->get('user_id'),
                 'ticket_id' => $query['ticket_id'],
-                'created_at'=>$query['date'],
-                'updated_at'=>$query['date'],
+                'created_at' => $query['date'],
+                'updated_at' => $query['date'],
             ]);
         }
 
