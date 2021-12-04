e50c0ab791b8567bc5fc2f23f96a1d903d9c73f3
Bighsueh <40841121@gm.nfu.edu.tw>
2021-12-04 21:52:00 +0800

    完成 滾邊,剪巾回報頁面

--- resources/views/pages/employee/TaskReportModal.blade.php
+++ resources/views/pages/employee/TaskReportModal.blade.php
@@ -1,121 +1,201 @@
-{{--    <div class="modal fade" id="TaskReportModal" tabindex="-1" role="dialog" aria-labelledby="TaskReportModalTitle"--}}
-{{--         aria-hidden="true">--}}
-<div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="TaskReportModalLongTitle">回報任務</h5>
-            <button type="button" class="close close-modal" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="modal-body">
-            <div class="container">
-                <div class="row form-group">
-                    <label for="report_ticket_id" class="col-sm-3 col-5 ">派遣單編號</label>
-                    <input class="form-control col-sm-8 col-7" id="report_ticket_id" type="text" readonly/>
-                </div>
-                <div class="row form-group">
-                    <label for="report_customer_name" class="col-sm-3 col-5 ">客戶名稱</label>
-                    <input class="form-control col-sm-8 col-7" id="report_customer_name" type="text" readonly/>
-                </div>
-                <div class="row form-group">
-                    <label for="report_color_cable" class="col-sm-3 col-5 ">色線編號</label>
-                    <input class="form-control col-sm-8 col-7" id="report_color_cable" type="text" readonly/>
-                </div>
-                <div class="row form-group">
-                    <label for="report_wash_tag" class="col-sm-3 col-5 ">洗標</label>
-                    <input class="form-control col-sm-8 col-7" id="report_wash_tag" type="text" readonly/>
-                </div>
-                <br>
-                <div class="form-group">
-                    <label class="col-sm-12 col-12">滾邊員完成數量</label>
-                    <table id="report_piping_list">
-                        <tr class="col-sm-12 col-12 piping-row row">
-                            <td class="piping-people col-sm-4 col-12 mr-auto mb-2 form-group">
-                                <select class="form-control ">
-                                    <option value="ppp" selected>滾邊員</option>
-                                    <option value="yes">耶斯</option>
-                                </select>
-                            </td>
-                            <td class="piping-number col-sm-4 col-6 mx-auto mb-2 form-group">
-                                <input class="form-control " id="report_piping_complete_num"
-                                       placeholder="請輸入數量" type="text"/>
-                            </td>
-                            <td class="piping-unit col-sm-4 col-6 ml-auto mb-2 form-group">
-                                <select class="form-control">
-                                    <option value="" selected>單位</option>
-                                    <option value="one">條</option>
-                                    <option value="dozen">打</option>
-                                </select>
-                            </td>
-
-                        </tr>
-
-                    </table>
-                    <div class="col-sm-12 col-12 row">
-                        <button class="btn col-sm-12 col-12 text-center btn-outline-info ml-autos"
-                                id="btn_add_pipings">+
-                        </button>
-                    </div>
-                </div>
-                <div class="row form-group">
-                    <label class="col-sm-12 col-12">剪巾員完成數量</label>
-                    <div class="col-sm-12 col-12 row">
-                        <input id="report-operator-name" class="form-control col-sm-11 col-11 mx-auto mb-2"
-                               value="剪巾name" readonly/>
-                    </div>
-                    <div class="col-sm-12 col-12 row">
-                        <input class="form-control col-sm-7 col-7 mx-auto" id="report_operator_num"
-                               placeholder="請輸入數量" type="text"/>
-                        <select id="report-operator-unit" class="form-control col-sm-3 col-3 mx-auto">
-                            <option value="one" selected>條</option>
-                            <option value="dozen">打</option>
-                        </select>
-                    </div>
-                </div>
-                <br>
-                <div class="row form-group mx-auto">
-                    <label for="report_time" class="col-sm-4 col-4">最後回報時間</label>
-                    <input class="form-control col-sm-7 col-7" id="report_time" type="text" readonly/>
-                </div>
+<div class="modal fade" id="TaskReportModal" tabindex="-1" role="dialog" aria-labelledby="TaskReportModalTitle"
+     aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="TaskReportModalLongTitle">回報任務</h5>
+                <button type="button" class="close close-modal" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
+            <div class="modal-body">
+                <div class="container">
+                    <div class="row form-group">
+                        <label for="report_ticket_id" class="col-sm-3 col-5 ">派遣單編號</label>
+                        <input class="form-control col-sm-8 col-7" id="report_ticket_id" type="text" readonly/>
+                    </div>
+                    <div class="row form-group">
+                        <label for="report_customer_name" class="col-sm-3 col-5 ">客戶名稱</label>
+                        <input class="form-control col-sm-8 col-7" id="report_customer_name" type="text" readonly/>
+                    </div>
+                    <div class="row form-group">
+                        <label for="report_color_cable" class="col-sm-3 col-5 ">色線編號</label>
+                        <input class="form-control col-sm-8 col-7" id="report_color_cable" type="text" readonly/>
+                    </div>
+                    <div class="row form-group">
+                        <label for="report_wash_tag" class="col-sm-3 col-5 ">洗標</label>
+                        <input class="form-control col-sm-8 col-7" id="report_wash_tag" type="text" readonly/>
+                    </div>
+                    <br>
+                    <div class="form-group">
+                        <label class="col-sm-12 col-12">滾邊員完成數量</label>
+                        <table id="report_piping_list">
+                            <tr class="col-sm-12 col-12 piping-row row">
+                                <td class="piping-people col-sm-4 col-12 mr-auto mb-2 form-group">
+                                    <select class="form-control piping-select">
+                                        <option value="">滾邊員</option>
+                                    </select>
+                                </td>
+                                <td class="piping-number col-sm-4 col-6 mx-auto mb-2 form-group">
+                                    <input class="form-control " id="report_piping_complete_num"
+                                           placeholder="請輸入數量" type="text"/>
+                                </td>
+                                <td class="piping-unit col-sm-4 col-6 ml-auto mb-2 form-group">
+                                    <select class="form-control">
+                                        <option value="" selected>單位</option>
+                                        <option value="one">條</option>
+                                        <option value="dozen">打</option>
+                                    </select>
+                                </td>
+
+                            </tr>
+
+                        </table>
+                        <div class="col-sm-12 col-12 row">
+                            <button class="btn col-sm-12 col-12 text-center btn-outline-info ml-autos"
+                                    id="btn_add_pipings">+
+                            </button>
+                        </div>
+                    </div>
+                    <div class="row form-group">
+                        <label class="col-sm-12 col-12">剪巾員完成數量</label>
+                        <div class="col-sm-12 col-12 row">
+                            <input id="report-operator-name" class="form-control col-sm-11 col-11 mx-auto mb-2"
+                                   value="剪巾name" readonly/>
+                        </div>
+                        <div class="col-sm-12 col-12 row">
+                            <input class="form-control col-sm-7 col-7 mx-auto" id="report_operator_num"
+                                   placeholder="請輸入數量" type="text"/>
+                            <select id="report-operator-unit" class="form-control col-sm-3 col-3 mx-auto">
+                                <option value="one" selected>條</option>
+                                <option value="dozen">打</option>
+                            </select>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row form-group mx-auto">
+                        <label for="report_time" class="col-sm-4 col-4">最後回報時間</label>
+                        <input class="form-control col-sm-7 col-7" id="report_time" type="text" readonly/>
+                    </div>
+                </div>
 
 
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-outline-success" id="btn_store_report">送出回報</button>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-outline-success" id="btn_store_report">送出回報</button>
+            </div>
         </div>
     </div>
 </div>
-{{--    </div>--}}
 <script>
-
     //滾邊員增加
-    let create_piping_row = $("#report_piping_list").html();
+    let temp_piping_row;
+    let emtpy_piping_row = $("#report_piping_list").html();
 
     //增加滾邊員輸入列
     $("#btn_add_pipings").click(function () {
-        temp_piping_row = $("#report_piping_list").html();
-        $("#report_piping_list").html(temp_piping_row + create_piping_row);
+        create_piping_row();
     });
 
+    //產生新滾邊員列
+    function create_piping_row() {
+        $("#report_piping_list").append(temp_piping_row);
+    }
+
+    //格式化滾邊員列
+    function format_piping_row() {
+        $("#report_piping_list").html(emtpy_piping_row)
+    }
 
     //打開report_modal(傳入ticket_id)
-    function open_report_modal(ticket_id) {
-        {{--$.ajax({--}}
-        {{--    url: '{{route('')}}',--}}
-        {{--    method: 'get',--}}
-        {{--    data: {--}}
+    function open_report_modal(ticket_id = 1) {
+        url = "{{route('get_report_data')}}";
+        $.ajax({
+            url: url,
+            method: 'GET',
+            data: {
+                ticket_id: ticket_id,
+                action: '剪巾',
+            },
+            success: function (res) {
+                set_report_modal(res);
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        })
 
-        {{--    },--}}
-        {{--    success: function (res) {--}}
+    }
 
-        {{--        $("#TaskReportModal").modal('show');--}}
-        {{--    },--}}
-        {{--    error: function (err) {--}}
+    //設置回報頁欄位資訊
+    function set_report_modal(data) {
+        //派遣單編號
+        document.getElementById('report_ticket_id').value =
+            data['ticket_reports'][0]['ticket_id'];
+        //客戶名稱
+        document.getElementById('report_customer_name').value =
+            data['ticket_reports'][0]['employeeName'];
+        //色線編號
+        document.getElementById('report_color_cable').value =
+            data['ticket_reports'][0]['colorId'] + data['ticket_reports'][0]['colorId2'];
+        //洗標
+        document.getElementById('report_wash_tag').value =
+            data['ticket_reports'][0]['wash'];
 
-        {{--    },--}}
-        {{--})--}}
+        //剪巾員名稱
+        document.getElementById('report-operator-name').value =
+            data['ticket_reports'][0]['name'];
+        //剪巾員回報數量
+        document.getElementById('report_operator_num').value =
+            data['ticket_reports'][0]['quantity'];
+        //剪巾員回報數量之單位
+        document.getElementById('report-operator-unit').value =
+            data['ticket_reports'][0]['unit'];
+
+        //最後回報時間
+        document.getElementById('report_time').value =
+            data['ticket_reports'][0]['updated_at']
+
+        //先把滾邊員列格式化
+        format_piping_row();
+
+        //滾邊員選項填入
+        $(".piping-select").empty();
+        $.each(data['piping_members'], function (index, row) {
+            $(".piping-select").append(
+                "<option value=" + row['id'] + ">" + row['name'] + "</option>"
+            );
+        })
+
+        //設定動態產生用的空白列
+        temp_piping_row = $("#report_piping_list").html();
+
+        //將資料寫入滾邊員列表內
+        let piping_last_row = $("#report_piping_list tr").last().children();
+        let piping_last_people = piping_last_row.children('.piping-people > select');
+        let piping_last_number = piping_last_row.children('.piping-number > input');
+        let piping_last_unit = piping_last_row.children('.piping-unit > select');
+
+
+        //因為第一列已經存在所以直接寫入資料
+        piping_last_number.val(data["piping_reports"][0]["quantity"]);
+        piping_last_unit.val(data["piping_reports"][0]["unit"]);
+        for (let i = 1; i < data["piping_reports"].length; i++) {
+            //產生新的一列
+            create_piping_row();
+            //重新定義最後一行
+            piping_last_row = $("#report_piping_list tr").last().children();
+            piping_last_people = piping_last_row.children('.piping-people > select');
+            piping_last_number = piping_last_row.children('.piping-number > input');
+            piping_last_unit = piping_last_row.children('.piping-unit > select');
+            //寫入資料
+            piping_last_people.val(data["piping_reports"][i]["operator"]);
+            piping_last_number.val(data["piping_reports"][i]["quantity"]);
+            piping_last_unit.val(data["piping_reports"][i]["unit"]);
+
+        }
+
+
         $("#TaskReportModal").modal('show');
     }
 
