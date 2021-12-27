f5aed4a0447a82d2f9ab4a7a24e72f1a775edff8
Hsueh <40841121@gm.nfu.edu.tw>
2021-12-27 15:28:47 +0800

    修正 員工工作報表 excel輸出 查詢邏輯

--- app/Exports/JobReportExport.php
+++ app/Exports/JobReportExport.php
@@ -29,22 +29,25 @@ class JobReportExport implements FromView
         if ($this->search_parameter !== $search_parameter) {
             $search_parameter = $this->search_parameter;
         }
+
         $data = DB::table('job_reports')
-            ->where('operator', $this->employee_id)
-            ->join('job_tickets', 'job_tickets.id', '=', 'job_reports.ticket_id')
-            ->where('job_tickets.status', '出貨')
-            ->orWhere('job_tickets.status', 'like', '%' . $search_parameter . '%')
-            ->orWhere('job_tickets.id', 'like', '%' . $search_parameter . '%')
-            ->orWhere('job_tickets.updated_at', 'like', '%' . $search_parameter . '%')
-            ->orWhere('job_tickets.employeeName', 'like', '%' . $search_parameter . '%')
-            ->orWhere('job_tickets.itemId', 'like', '%' . $search_parameter . '%')
-            ->orWhere('job_tickets.rollFunc', 'like', '%' . $search_parameter . '%')
-            ->orWhere('job_tickets.item', 'like', '%' . $search_parameter . '%')
-            ->orWhere('job_reports.quantity', 'like', '%' . $search_parameter . '%')
-            ->orWhere('job_reports.unit', 'like', '%' . $search_parameter . '%')
-            ->get();
+            ->join('job_tickets', 'job_tickets.id', '=', 'job_reports.ticket_id');
 
-
+        if ($search_parameter) {
+            $data = $data->where(function ($query) use ($search_parameter){
+                $query->orWhere('job_tickets.status', 'like binary', '%' . $search_parameter . '%')
+                    ->orWhere('job_tickets.id', 'like binary', '%' . $search_parameter . '%')
+                    ->orWhere('job_tickets.updated_at', 'like binary', '%' . $search_parameter . '%')
+                    ->orWhere('job_tickets.employeeName', 'like binary', '%' . $search_parameter . '%')
+                    ->orWhere('job_tickets.itemId', 'like binary', '%' . $search_parameter . '%')
+                    ->orWhere('job_tickets.rollFunc', 'like binary', '%' . $search_parameter . '%')
+                    ->orWhere('job_tickets.item', 'like binary', '%' . $search_parameter . '%')
+                    ->orWhere('job_reports.quantity', 'like binary', '%' . $search_parameter . '%')
+                    ->orWhere('job_reports.unit', 'like binary', '%' . $search_parameter . '%')
+                    ->orWhere('job_reports.action', 'like binary', '%' . $search_parameter . '%');
+            });
+        }
+        $data = $data->where('job_reports.operator', '=', $this->employee_id)->get();
 
         return view('pages.exports.jobReport', [
             'data' => $data
